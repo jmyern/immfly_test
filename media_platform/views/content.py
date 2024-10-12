91f58554fe371ee5fed71437a1acfabd3a9fd107
Joan Mari <jmyern90@gmail.com>
2024-10-12 12:41:54 +0200

    finish Content retrieval

--- media_platform/views/content.py
+++ media_platform/views/content.py
@@ -1,4 +1,5 @@
 from media_platform.models import Content
+from media_platform.models import Channel
 from media_platform.serializers import ContentSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
@@ -8,11 +9,13 @@ class ContentList(generics.ListAPIView):
     """
     Queryset that retrieves and filters channels of the highest level (no other channel uses this channel as subchannel)
     """
-    # def get_queryset(self):
-    #     # Filter all channels that have sub_channels
-    #     subchannels = Channel.objects.exclude(sub_channels__isnull=True).all().values_list("sub_channels", flat=True)
-    #     return Channel.objects.exclude(id__in=subchannels).all()
-    queryset = Content.objects.all()
+    def get_queryset(self):
+        # Filter all content for given channel
+        channel_id = self.kwargs["channel_id"]
+        channel = Channel.objects.filter(id=channel_id)[0]
+
+        return channel.content.all()
+
     serializer_class = ContentSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['rating']
+    filterset_fields = ['id', 'rating']
